f89438c05b2104cb3fbe9bd867bd77188ce6ff5c
Kaushal Faldu <85346182+kaushal1610@users.noreply.github.com>
2022-02-14 12:16:46 +0530

    Add files via upload

---
+++ Assignment 19.py
@@ -0,0 +1,17 @@
+def unique_file(input_filename, output_filename):
+    input_file = open(input_filename, 'r')
+    file_contents = input_file.read()
+    input_file.close()
+    duplicates = []
+    word_list = file_contents.split()
+    file = open(output_filename, 'w')
+    for word in word_list:
+        if word not in duplicates:
+            duplicates.append(word)
+            file.write(str(word) + "\n")
+    file.close()
+
+
+unique_file('sample.txt','unique_output.txt')
+for line in sorted(open('unique_output.txt')):
+    print(line, end='')
